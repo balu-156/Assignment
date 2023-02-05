4ce14675696f281e57411099524db1b55e233736
balu-156 <balajiedamakanti9@gmail.com>
2023-02-06 00:46:22 +0530

    2 commit

---
+++ src/strings/CharAt.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class CharAt {
+    public static void main(String[] args){
+        String name="javatpoint";
+        char ch=name.charAt(3);
+        System.out.println(ch);
+    }
+}

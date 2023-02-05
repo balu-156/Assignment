a6a5cf3e9ec2414f23a5d6f67136907fba0a7e18
balu-156 <balajiedamakanti9@gmail.com>
2023-02-06 01:05:59 +0530

    12 commit

---
+++ src/strings/IndexOfExample.java
@@ -0,0 +1,19 @@
+package strings;
+
+public class IndexOfExample {
+    public static void main(String args[]){
+        String s1="this is index of example";
+//passing substring
+        int index1=s1.indexOf("is");
+        int index2=s1.indexOf("index");
+        System.out.println(index1+"  "+index2);
+
+//passing substring with from index
+        int index3=s1.indexOf("is",4);
+        System.out.println(index3);
+
+//passing char value
+        int index4=s1.indexOf('s');
+        System.out.println(index4);
+    }
+}
